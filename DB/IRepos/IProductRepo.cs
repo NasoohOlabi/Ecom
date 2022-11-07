faee20f39dbb911cb1df1fe943b13a1f7da4cdad
NasoohOlabi <nasooholabi@gmail.com>
2022-11-07 23:44:07 +0200

    Create Product Repo

---
+++ DB/IRepos/IProductRepo.cs
@@ -0,0 +1,14 @@
+﻿using DB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB.IRepos
+{
+    public interface IProductRepo : IBaseRepo<Product>
+    {
+        public void UpdateRating(long id,long count, long sum);
+    }
+}
---
+++ DB/Repos/ProductRepo.cs
@@ -0,0 +1,40 @@
+﻿using DB.IRepos;
+using DB.Models;
+using DB.UOW;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB.Repos
+{
+    public class ProductRepo : BaseRepo<Product>, IProductRepo
+    {
+
+        private readonly ILogger<ProductRepo> _logger;
+
+
+
+        public ProductRepo(EComContext db, ILogger<ProductRepo> logger) : base(db)
+        {
+            _logger = logger;
+        }
+
+        public void UpdateRating(long id,long count, long sum)
+        {
+            try
+            {
+                var prod = _db.Products.Single(x => x.Id == id);
+                prod.RatingSum = sum;
+                prod.RatingCount = count;
+            }
+            catch (InvalidOperationException)
+            {
+                string message = $"A Product with id {id} Was Deleted before Rating Was Updated to count {count} and sum {sum}";
+                _logger.LogDebug(message);
+            }
+        }
+    }
+}
