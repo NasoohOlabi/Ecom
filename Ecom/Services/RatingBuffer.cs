401d2b41da8e63d945ba8b40071a61f75c88245d
NasoohOlabi <nasooholabi@gmail.com>
2022-11-07 23:45:10 +0200

    Intorduce In Memory Buffer for hot Products Buffering

---
+++ Ecom/Services/RatingBuffer.cs
@@ -0,0 +1,46 @@
+﻿using DB.IRepos;
+using DB.Models;
+using DB.UOW;
+
+namespace Ecom.Services
+{
+    public interface IRatingBuffer
+    {
+        public void AddRating(Rating rating,IProductRepo productRepo);
+    }
+    public class RatingBuffer : IRatingBuffer
+    {
+        /**
+         * ProductId -> (Count,RatingSum
+         */
+        private readonly IDictionary<long,(long,long)> _buffer;
+        public RatingBuffer()
+        {
+            _buffer = new Dictionary<long, (long, long)>();
+        }
+        public void AddRating(Rating rating,IProductRepo productRepo)
+        {
+            if (_buffer.Count > 10000)
+            {
+                foreach (var entry in _buffer)
+                {
+                    var (c, s) = entry.Value;
+                    productRepo.UpdateRating(entry.Key, c, s, message);
+                }
+                _buffer.Clear();
+            }
+            else
+            {
+                if (_buffer.ContainsKey(rating.ProductId))
+                {
+                    var (count,rSum) = _buffer[rating.ProductId];
+                    _buffer[rating.ProductId] = (count+1,rSum + rating.Rate);
+                }
+                else
+                {
+                    _buffer.Add(rating.ProductId, (1, rating.Rate));
+                }
+            }
+        }
+    }
+}
