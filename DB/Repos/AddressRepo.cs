05c6f8333b9edd5a441b9495d8285a76300448ad
NasoohOlabi <nasooholabi@gmail.com>
2022-12-06 13:26:58 +0300

    Address and Specification repo init

---
+++ DB/Repos/AddressRepo.cs
@@ -0,0 +1,40 @@
+﻿using DB.IRepos;
+using DB.Models;
+using DB.UOW;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB.Repos
+{
+    public class AddressRepo : BaseRepo<Product>, IAddressRepo
+    {
+
+        private readonly ILogger<AddressRepo> _logger;
+
+
+
+        public AddressRepo(EComContext db, ILogger<AddressRepo> logger) : base(db)
+        {
+            _logger = logger;
+        }
+
+        public void UpdateRating(long id, long count, long sum)
+        {
+            try
+            {
+                var prod = _db.Products.Single(x => x.Id == id);
+                prod.RatingSum = sum;
+                prod.RatingCount = count;
+            }
+            catch (InvalidOperationException)
+            {
+                string message = $"A Product with id {id} Was Deleted before Rating Was Updated to count {count} and sum {sum}";
+                _logger.LogDebug(message);
+            }
+        }
+    }
+}
---
+++ DB/Repos/SpecificationRepo.cs
@@ -0,0 +1,27 @@
+﻿using DB.IRepos;
+using DB.Models;
+using DB.UOW;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB.Repos
+{
+    public class SpecificationRepo : BaseRepo<Specification>, ISpecificationRepo
+    {
+
+        private readonly ILogger<ProductRepo> _logger;
+
+
+
+        public SpecificationRepo(EComContext db, ILogger<ProductRepo> logger) : base(db)
+        {
+            _logger = logger;
+        }
+
+        
+    }
+}
