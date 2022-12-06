0951c59b18273fa75d6aec9c2170682dc2c5f353
Nour <nouracm7@gmail.com>
2022-12-06 12:42:09 +0200

    viewmodels

---
+++ Ecom/Models/CategoryViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecom.Models
+{
+    public partial class CategoryDetailsViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        [Display(Name = "Created At")]
+        public DateTime CreatedAt { get; set; }
+        [Display(Name = "Modified At")]
+        public DateTime ModifiedAt { get; set; }
+
+    }
+
+    public partial class CategoryEditViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+
+    }
+
+}
+
+
