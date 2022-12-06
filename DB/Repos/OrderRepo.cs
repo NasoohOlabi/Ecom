b1f42cb4940f0647a253b1aa1e7382ada2ffc6d5
NasoohOlabi <nasooholabi@gmail.com>
2022-12-06 13:36:48 +0300

    Coupon Order Notification Repos

---
+++ DB/Repos/OrderRepo.cs
@@ -0,0 +1,27 @@
+﻿using DB.IRepos;
+using DB.Models;
+using DB.UOW;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB.Repos
+{
+    public class OrderRepo : BaseRepo<Order>, IOrderRepo
+    {
+
+        private readonly ILogger<ProductRepo> _logger;
+
+
+
+        public OrderRepo(EComContext db, ILogger<ProductRepo> logger) : base(db)
+        {
+            _logger = logger;
+        }
+
+        
+    }
+}
