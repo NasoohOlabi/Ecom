acf2f6d46da118b60f478a2123b895c95f61e90e
NasoohOlabi <nasooholabi@gmail.com>
2022-12-07 10:06:36 +0300

    Add repositories to UOW

--- DB/UOW/UnitOfWork.cs
+++ DB/UOW/UnitOfWork.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DB.IRepos;
 using DB.Models;
+using DB.Repos;
+using Microsoft.Extensions.Logging;
 
 namespace DB.UOW
 {
@@ -11,6 +14,64 @@ namespace DB.UOW
     {
 
         protected readonly EComContext _db;
+        protected readonly ILogger<UnitOfWork>? _logger;
+
+        private IProductRepo _product;
+        public IProductRepo Products
+        {
+            get
+            {
+                _product ??= new ProductRepo(_db);
+                return _product;
+            }
+        }
+        private IAddressRepo? _address = null;
+        public IAddressRepo Adresses
+        {
+            get
+            {
+                _address ??= new AddressRepo(_db);
+                return _address;
+            }
+        }
+        private ISpecificationRepo? _specification = null;
+        public ISpecificationRepo Specifications
+        {
+            get
+            {
+                _specification ??= new SpecificationRepo(_db);
+                return _specification;
+            }
+        }
+        
+        private IOrderRepo? _order = null;
+        public IOrderRepo Orders
+        {
+            get
+            {
+                _order ??= new OrderRepo(_db);
+                return _order;
+            }
+        }
+        private INotificationRepo? _notification = null;
+        public INotificationRepo Notifications
+        {
+            get
+            {
+                _notification ??= new NotificationRepo(_db);
+                return _notification;
+            }
+        }
+        private ICouponRepo? _coupon = null;
+        public ICouponRepo Coupons
+        {
+            get
+            {
+                _coupon ??= new CouponRepo(_db);
+                return _coupon;
+            }
+        }
+
 
         public UnitOfWork(EComContext db)
         {
