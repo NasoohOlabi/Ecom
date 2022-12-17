bcc404f172765d8993655ae2e827a88016da2ef9
NasoohOlabi <nasooholabi@gmail.com>
2022-12-17 10:11:51 +0300

    Stream line js as much as it can be.

--- Ecom/wwwroot/js/site.js
+++ Ecom/wwwroot/js/site.js
@@ -2,3 +2,54 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function htmlDecode(input) {
+  var doc = new DOMParser().parseFromString(input, "text/html");
+  return doc.documentElement.textContent;
+}
+
+function getElementFromString(string) {
+  let div = document.createElement("div");
+  div.innerHTML = string;
+  return div.firstElementChild;
+}
+
+function parseModel(str) {
+  window.Model = JSON.parse(htmlDecode(str))
+}
+
+function getRenderer({
+  targetSelector
+  , targetElementsSelector
+  , modelToTargetData
+  , targetDataIdentity
+  , targetElementGenerator
+  , targetSelectionCustomizer
+  , sourceSelector
+  , sourceElementsSelector
+  , modelToSourceData
+  , sourceDataIdentity
+  , sourceElementGenerator
+  , sourceSelectionCustomizer
+}) {
+  return () => {
+    const targetSelection = d3.select(targetSelector)
+      .selectAll(targetElementsSelector)
+      .data(modelToTargetData(Model), targetDataIdentity);
+    targetSelection
+      .enter()
+      .append(targetElementGenerator);
+    targetSelection
+      .exit().remove();
+    targetSelectionCustomizer(targetSelection);
+    const sourceSelection = d3.select(sourceSelector)
+      .selectAll(sourceElementsSelector)
+      .data(modelToSourceData(Model), sourceDataIdentity);
+    sourceSelection
+      .enter()
+      .append(sourceElementGenerator);
+    sourceSelection
+      .exit().remove();
+    sourceSelectionCustomizer(sourceSelection);
+  }
+}
